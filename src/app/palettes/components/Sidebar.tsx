68527b8777402eeee57d30814c9f5be3bc80dfba
Aman Ghanghoriya <121393968+amonsoon@users.noreply.github.com>
2023-05-09 22:44:56 +0530

    update

--- src/app/palettes/components/Sidebar.tsx
+++ src/app/palettes/components/Sidebar.tsx
@@ -16,10 +16,12 @@ function Sidebar({}: Props) {
 
   return (
     <div className={`${css.sidebar} ${!isSidebar ? css["sidebar--closed"] : ""}`}>
+      <div className={css.sidebar__header}>
+        <button className={css.sidebar__close} onClick={toggleSidebar}>
+          <ChevronLeft size={18} />{" "}
+        </button>
+      </div>
       <div className={css.sidebar__wrapper}>
-        <div className={css.sidebar__header}> 
-          <button className={css.sidebar__close} onClick={toggleSidebar}> <ChevronLeft size={18} /> </button>
-        </div>
         <div className={`${css.sidebar__nav} ${css.nav}`}>
           <div className={css.nav__item}>
             <Link
