1f23d1c72b5db05ca7630ab7b6f13ac8730ab5d5
Aman Ghanghoriya <121393968+amonsoon@users.noreply.github.com>
2023-05-17 17:30:49 +0530

    update

---
+++ src/app/create-palette/[slug]/NewPalette.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import css from './style.module.css'
+
+type Props = {}
+
+function NewPalette({}: Props) {
+  return (
+    <div className={css.paletee}>
+
+
+    </div>
+  )
+}
+
+export default NewPalette
---
+++ src/app/create-palette/[slug]/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NewPalette from './NewPalette'
+
+type Props = {}
+
+function page({}: Props) {
+  return (
+    <div>
+      <NewPalette />
+    </div>
+  )
+}
+
+export default page
