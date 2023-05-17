8e3de3ce7f0ce1092e64f84f6722cbcc2fe478e8
Aman Ghanghoriya <121393968+amonsoon@users.noreply.github.com>
2023-05-17 20:19:55 +0530

    update

--- src/app/create-palette/[slug]/NewPalette.tsx
+++ src/app/create-palette/[slug]/NewPalette.tsx
@@ -1,13 +1,116 @@
-import React from 'react'
+'use client'
+
+import { useState } from 'react';
+import { DragDropContext, Droppable, Draggable } from '@hello-pangea/dnd'
+
 import css from './style.module.css'
+
+
+// a little function to help us with reordering the result
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
+
+const grid = 8;
+
+const getItemStyle = (isDragging : boolean, draggableStyle) => ({
+  // some basic styles to make the items look a bit nicer
+  userSelect: 'none',
+  padding: grid * 2,
+  marginRight: 10,
+  // change background colour if dragging
+  background: isDragging ? 'lightgreen' : 'grey',
+  // styles we need to apply on draggables
+  ...draggableStyle,
+});
+
+const getListStyle = (isDraggingOver : boolean) => ({
+  background: isDraggingOver ? 'lightblue' : 'lightgrey',
+  display: 'flex',
+  padding: grid,
+  overflow: 'auto',
+});
 
 type Props = {}
 
-function NewPalette({}: Props) {
+type Item = {
+  id: number,
+  content: string,
+}
+
+function NewPalette({ }: Props) {
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      content: 'good'
+    },
+    {
+      id: 2,
+      content: 'nice'
+    },
+    {
+      id: 3,
+      content: 'best'
+    },
+    {
+      id: 4,
+      content: 'he'
+    },
+  ])
+
+  const onDragEnd = (result) => {
+    // dropped outside the list
+    if (!result.destination) {
+      return;
+    }
+
+    const updatedItems = reorder(
+      items,
+      result.source.index,
+      result.destination.index
+    ) as Item[];
+
+    setItems(updatedItems);
+  }
+
+
   return (
-    <div className={css.paletee}>
-
-
+    <div>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="droppable" direction="horizontal" >
+          {(provided, snapshot) => (
+            <div
+              ref={provided.innerRef}
+              style={getListStyle(snapshot.isDraggingOver)}
+              {...provided.droppableProps}
+            >
+              {items.map((item, index) => (
+                <Draggable key={item.id} draggableId={item.id.toString()} index={index} >
+                  {(provided, snapshot) => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      style={getItemStyle(
+                        snapshot.isDragging,
+                        provided.draggableProps.style
+                      )}
+                      className={css.palette__color}
+                    >
+                      {item.id}
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   )
 }
