1cc8bf6d45beb66bde53becfcac3d8b2a66a59e8
Aman Ghanghoriya <121393968+amonsoon@users.noreply.github.com>
2023-06-09 10:32:42 +0530

    update

--- src/app/create-palette/[slug]/NewPalette.tsx
+++ src/app/create-palette/[slug]/NewPalette.tsx
@@ -4,7 +4,8 @@ import { useState } from 'react';
 import { DragDropContext, Droppable, Draggable } from '@hello-pangea/dnd'
 
 import css from './style.module.css'
-import { Copy, Move, Grid } from 'react-feather';
+import { Copy, Move, Unlock, Lock, Heart, X, Trash } from 'react-feather';
+import { getIconColor } from '@/app/utils/color';
 
 
 // a little function to help us with reordering the result
@@ -47,29 +48,33 @@ type Item = {
   hex: string,
 }
 
+const myitems: Item[] = [
+  {
+    id: 1,
+    content: 'good',
+    hex: '#e3ffea'
+  },
+  {
+    id: 2,
+    content: 'nice',
+    hex: '#24fc03'
+  },
+  {
+    id: 3,
+    content: 'best',
+    hex: '#ff0095'
+  },
+  {
+    id: 4,
+    content: 'he',
+    hex: '#0d00ff'
+  },
+]
+
+
+
 function NewPalette({ }: Props) {
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      content: 'good',
-      hex: '#fcba03'
-    },
-    {
-      id: 2,
-      content: 'nice',
-      hex: '#24fc03'
-    },
-    {
-      id: 3,
-      content: 'best',
-      hex: '#ff0095'
-    },
-    {
-      id: 4,
-      content: 'he',
-      hex: '#0d00ff'
-    },
-  ])
+  const [items, setItems] = useState<Array<Item>>(myitems)
 
   const onDragEnd = (result) => {
     // dropped outside the list
@@ -119,11 +124,7 @@ function NewPalette({ }: Props) {
                       className={`${css.palette__color} ${css.color}`}
                       onClick={() => handleClick(index)}
                     >
-                      <div className={`${css.color__options}`}>
-                        <button className={css.color__btn}> <Move size={18} strokeWidth={1.5} />  </button>
-                        <button className={css.color__btn}> <Copy size={18} strokeWidth={1.5} />  </button>
-                        <button className={css.color__btn}> <Grid size={18} strokeWidth={1.5} />  </button>
-                      </div>
+                      <PaletteItem item={item} />
                     </div>
                   )}
                 </Draggable>
@@ -136,5 +137,28 @@ function NewPalette({ }: Props) {
     </div>
   )
 }
+
+
+type PaletteItemProps = {
+  item: Item
+}
+
+const PaletteItem = ({ item }: PaletteItemProps) => {
+  const { id, hex, content } = item;
+  const [isLocked, setLocked] = useState(false);
+
+  return (
+    <div className={`${css.color__options}`} style={{ backgroundColor: hex, color: 'black' }}>
+      <button className={css.color__btn}> <Trash size={18} strokeWidth={1.5} />  </button>
+      <button className={css.color__btn}> <Move size={18} strokeWidth={1.5} />  </button>
+      <button className={css.color__btn}> <Heart size={18} strokeWidth={1.5} />  </button>
+      <button className={css.color__btn}> <Copy size={18} strokeWidth={1.5} />  </button>
+      <button className={css.color__btn} onClick={() => setLocked(!isLocked)} >
+        {isLocked ? <Lock size={18} strokeWidth={1.5} /> : <Unlock size={18} strokeWidth={1.5} />}
+      </button>
+    </div>
+  )
+}
+
 
 export default NewPalette
